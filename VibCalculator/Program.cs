89c0df64f322b1238b35c4310411ff7e4afa6f66
Максим Богащенко <sergeyivanov85@yandex.ru>
2022-12-07 19:38:19 +0300

    Добавьте файлы проекта.

---
+++ VibCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VibroMath;
+
+namespace VibCalculator {
+    class Program {
+        static void Main(string[] args) {
+            Voltage voltage = new Voltage();
+            voltage.SetPIK_PIK(242);
+            double volt = voltage.GetPIK();
+        }
+    }
+}
---
+++ VibroMath/Parametrs/Parametr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibroMath {
+    public class Parametr {
+        private protected double Value;
+        //protected Parametr(double value) {
+        //    Value = value;
+        //}
+    }
+    public class SignalsParameter : Parametr {
+        //protected SignalsParameter(double RMSValue)
+        //    : base(RMSValue) {
+        //}
+        public void SetRMS(double RMS) {
+            Value = RMS;
+        }
+        public void SetPIK(double PIK) {
+            Value = PIK / Math.Sqrt(2);
+        }
+        public void SetPIK_PIK(double PIK_PIK) {
+            Value = PIK_PIK / 2 / Math.Sqrt(2);
+        }
+        public double GetRMS() {
+            return Value;
+        }
+        public double GetPIK() {
+            return Value* Math.Sqrt(2);
+        }
+        public double GetPIK_PIK() {
+            return Value * 2*Math.Sqrt(2);
+        }
+    }
+}
